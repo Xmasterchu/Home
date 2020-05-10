b2c9783b9dfc62209727f7562b84b3f64c223813
Viktor Bochkov <masterchu@live.ru>
2020-05-10 17:50:04 +0300

    Новая фраза

--- src/Home.java
+++ src/Home.java
@@ -6,7 +6,10 @@ public class Home {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         if (a.equals("Аня"))
+        {
+            System.out.println("Прекрасное имя");
             System.out.println(a + ",я тебя люблю");
+        }
         else System.out.println("Попробуйте еще");
         sc.close();
     }
