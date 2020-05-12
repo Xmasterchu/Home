e14cadcf30657a855c6b3a56cecf80a65bb4cbf9
Viktor Bochkov <masterchu@live.ru>
2020-05-12 20:24:24 +0300

    Новая фраза 2

--- src/Home.java
+++ src/Home.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Home {
     public static void main(String[] args) {
         System.out.println("Как вас зовут?");
+        System.out.println("Только не врите");
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         if (a.equals("Аня"))
