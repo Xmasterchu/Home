95438eb81a703ba35c43062f533f8a357775d41c
viktor bochkov <masterchu@live.ru>
2020-05-25 22:35:05 +0300

    цикл

---
+++ src/animals.java
@@ -0,0 +1,10 @@
+public class animals {
+    public static void main(String[] args) {
+        int a = 10 ;
+        while (a>=0);
+        {
+            System.out.println(a);
+            a--;
+        }
+    }
+}
